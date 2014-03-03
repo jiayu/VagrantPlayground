cd97adc4ad75ceeb9128f79a05e80a9767ad83d5
jiayu <jiayu.ji@gmail.com>
2014-03-03 14:19:33 -0600

    adding ssh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,23 @@
-sudo apt-get update
-sudo apt-get -y install openjdk-6-jdk openssh-server
+#sudo apt-get update
+#sudo apt-get -y install openjdk-6-jdk openssh-server
 
 
 #wget http://www.interior-dsgn.com/apache/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz 
-#tar -xzvf hadoop/hadoop-2.2.0.tar.gz
+#tar -xzvf hadoop-2.2.0.tar.gz
 
 #exporting enviroment variables
 echo "
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
 export HADOOP_HDFS_HOME=/home/vagrant/hadoop-2.2.0
 export HADOOP_COMMON_HOME=/home/vagrant/hadoop-2.2.0
 export HADOOP_YARN_HOME=/home/vagrant/hadoop-2.2.0
 export HADOOP_MAPRED_HOME=/home/vagrant/hadoop-2.2.0
-export PATH=$PATH:$HADOOP_COMMON_HOME/bin:$HADOOP_COMMON_HOME/sbin
+export PATH=$PATH:/home/vagrant/hadoop-2.2.0/bin:/home/vagrant/hadoop-2.2.0/sbin
 " >> /home/vagrant/.bashrc
 
-mkdir dfs
+#mkdir dfs
 
 #copy all the config files to hadoop
-cp  /vagrant/yarn-config/* /home/vagrant/hadoop-2.2.0/etc/hadoop
+#cp  /vagrant/yarn-config/* /home/vagrant/hadoop-2.2.0/etc/hadoop
 
 
