7c7ab555add9d0490b821ca375eb7179b8d6e455
jiayu <jiayu.ji@gmail.com>
2014-06-02 15:19:43 -0500

    runnable version. but require manually start hadoop in vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
-#sudo apt-get update
-#sudo apt-get -y install openjdk-6-jdk openssh-server
+#install java and ssh
+sudo apt-get update
+sudo apt-get -y install openjdk-6-jdk openssh-server
 
-
-#wget http://www.interior-dsgn.com/apache/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz 
-#tar -xzvf hadoop-2.2.0.tar.gz
+#download and extract hadoop
+wget -O hadoop.tar.gz http://www.interior-dsgn.com/apache/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz 
+tar -xzvf hadoop.tar.gz
+rm hadoop.tar.gz 
 
 #exporting enviroment variables
 echo "
@@ -15,9 +17,20 @@ export HADOOP_MAPRED_HOME=/home/vagrant/hadoop-2.2.0
 export PATH=$PATH:/home/vagrant/hadoop-2.2.0/bin:/home/vagrant/hadoop-2.2.0/sbin
 " >> /home/vagrant/.bashrc
 
-#mkdir dfs
-
 #copy all the config files to hadoop
-#cp  /vagrant/yarn-config/* /home/vagrant/hadoop-2.2.0/etc/hadoop
+cp  /vagrant/yarn-config/* /home/vagrant/hadoop-2.2.0/etc/hadoop
+chown -R vagrant:vagrant /home/vagrant/hadoop-2.2.0/
+
+#format namenode
+su vagrant -c "/home/vagrant/hadoop-2.2.0/bin/hadoop namenode -format"
+
+#configure ssh passwordless login
+mkdir -p /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant/.ssh
+su vagrant -c "ssh-keygen -t rsa -P '' -f /home/vagrant/.ssh/id_rsa"
+cat /home/vagrant/.ssh/id_rsa.pub >> .ssh/authorized_keys 
+
+
 
 
